715bc7b8466df5e0a45f364246fa4d214ae63e95
Sebastian Liu <sebastian.liu@gmail.com>
2015-10-21 22:57:18 -0700

    Fixes upon suggestions by the brilliant Jason Pang.

--- js/game.js
+++ js/game.js
@@ -1,4 +1,5 @@
-function Game() {}
+function Game() {
+}
 
 Game.prototype.initializeGame = function() {
   this.puzzles = new Puzzles();
@@ -11,8 +12,8 @@ Game.prototype.initializeGame = function() {
 Game.prototype.newGame = function() {
   this.initializeGame();
   this.puzzle = this.puzzles.getNewPuzzle();
-  this.firstNumber.number = this.firstNumber.number(this.puzzle[0]);
-  this.secondNumber.number = this.secondNumber.number(this.puzzle[1]);
-  this.thirdNumber.number = this.thirdNumber.number(this.puzzle[2]);
-  this.fourthNumber.number = this.fourthNumber.number(this.puzzle[3]);
+  this.firstNumber.number(this.puzzle[0]);
+  this.secondNumber.number(this.puzzle[1]);
+  this.thirdNumber.number(this.puzzle[2]);
+  this.fourthNumber.number(this.puzzle[3]);
 };
