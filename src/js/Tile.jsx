79433317d74b23d36a684b3325c88938611563d3
Sebastian Liu <sebastian.liu@gmail.com>
2015-11-26 20:44:18 -0800

    Tiles created from individual Tile elements.

--- src/js/Tile.jsx
+++ src/js/Tile.jsx
@@ -1,32 +1,38 @@
 import React from 'react';
 
 var Tile = React.createClass({
+
+  // All tiles are required to have some value.
+  propTypes: {
+    value: React.PropTypes.number.isRequired
+  },
+
+  // Initially tiles are not swappable or parenthesizable.
   getInitialState: function() {
     return {
-      value: '1',
       swappable: false,
       parenthesizable: false
     };
   },
 
+  // Method used to toggle swappable state.
   toggleSwappable: function() {
     this.setState({
       swappable: !this.state.swappable
     });
-    console.log('Swappable: ' + this.state.swappable);
   },
 
+  // Method used to toggle parenthesizable state.
   toggleParenthesizable: function() {
     this.setState({
       parenthesizable: !this.state.parenthesizable
     });
-    console.log('Parenthesizable: ' + this.state.parenthesizable);
   },
 
   render: function() {
     return (
       <div className="number-tile" onClick={this.toggleSwappable} onDoubleClick={this.toggleParenthesizable}>
-        <span className="unselectable number">{this.state.value}</span>
+        <span className="unselectable number">{this.props.value}</span>
       </div>
     )
   }
--- src/js/app.js
+++ src/js/app.js
@@ -2,13 +2,21 @@ import '../styles/site.scss';
 
 import Title from './Title.jsx'
 import Tile from './Tile.jsx'
+import Tiles from './Tiles.jsx'
 import Operator from './Operator.jsx'
 import HowToPlay from './HowToPlay.jsx'
 import Instructions from './Instructions.jsx'
 
 import React from 'react'
 
-React.render(<Title/>, document.getElementsByTagName('header')[0]);
+var currentPuzzle = [
+  {value: 1},
+  {value: 3},
+  {value: 4},
+  {value: 6}
+];
+
+React.render(<Title />, document.getElementsByTagName('header')[0]);
 React.render(<Operator/>, document.getElementById('game'));
-React.render(<Tile/>, document.getElementById('game'));
+React.render(<Tiles puzzle={currentPuzzle} />, document.getElementById('game'));
 React.render(<HowToPlay/>, document.getElementsByTagName('footer')[0]);
