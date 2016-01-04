395db16f9011626b62c7466a06a509ef54c70468
Jason Pang <jasonpang2011@gmail.com>
2016-01-03 20:18:30 -0800

    Minor edits
    - Modify gitignore to exclude public/ and *.css/*.css.map
    - ES6ified utils.js
    - Modified webpack config to include debugger and not mangle when
      minifying

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,43 +1,42 @@
-function Utils() {}
-
-Utils.loadJson = function(file) {
-  var json = null;
-  $.ajax({
-    'async': false,
-    'global': false,
-    'url': file,
-    'dataType': "json",
-    success: function(data) {
-      json = data;
-    }
-  });
-  return json;
-};
-
-Utils.cleanStringToCompute = function(stringToCompute) {
-  return stringToCompute.replace(/[^0-9\+\-\*\/\(\)]+/g, '');
-};
-
-Utils.cleanOperators = function(stringToClean) {
-  return stringToClean.replace(/×+/g, '*').replace(/÷+/g, '/').replace(/−+/g, '-');
-};
-
-Utils.getRandomValueInArray = function(array) {
-  var random = Math.floor(Math.random() * array.length);
-  return array[random];
-}
-
-Utils.randomize = function(array) {
-  for (var i = 0; i < array.length; i ++) {
-    var random = Math.floor(Math.random() * array.length);
-    this.swap(i, random, array);
+export default class Utils {
+  loadJson(file) {
+    var json = null;
+    $.ajax({
+      'async': false,
+      'global': false,
+      'url': file,
+      'dataType': "json",
+      success: function (data) {
+        json = data;
+      }
+    });
+    return json;
   }
-  return array;
-};
 
-// Helper method for randomize.
-Utils.swap = function(a, b, array) {
-  var temp = array[a];
-  array[a] = array[b];
-  array[b] = temp;
-};
+  cleanStringToCompute(stringToCompute) {
+    return stringToCompute.replace(/[^0-9\+\-\*\/\(\)]+/g, '');
+  }
+
+  cleanOperators(stringToClean) {
+    return stringToClean.replace(/×+/g, '*').replace(/÷+/g, '/').replace(/−+/g, '-');
+  }
+
+  getRandomValueInArray(array) {
+    var random = Math.floor(Math.random() * array.length);
+    return array[random];
+  }
+
+  randomize(array) {
+    for (var i = 0; i < array.length; i++) {
+      var random = Math.floor(Math.random() * array.length);
+      this.swap(i, random, array);
+    }
+    return array;
+  }
+
+  swap(a, b, array) {
+    var temp = array[a];
+    array[a] = array[b];
+    array[b] = temp;
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,14 @@ var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 var IS_PROD = false;
 
+Date.prototype.timeNow = function() {
+  var hours = this.getHours();
+  var ampm = (hours >= 12 ? 'PM' : 'AM');
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  return ((hours < 10) ? "0" : "") + hours + ":" + ((this.getMinutes() < 10) ? "0" : "") + this.getMinutes() + ":" + ((this.getSeconds() < 10) ? "0" : "") + this.getSeconds() + " " + ampm;
+};
+
 
 module.exports = {
   entry: [
@@ -37,24 +45,31 @@ module.exports = {
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.AggressiveMergingPlugin(),
     new webpack.optimize.UglifyJsPlugin({
-      sourceMap: false,
+      sourceMap: true,
       compress: {
-        sequences: true,
-        dead_code: true,
-        conditionals: true,
-        booleans: true,
-        unused: true,
-        if_return: true,
-        join_vars: true,
-        drop_console: true
+        sequences: false,
+        dead_code: false,
+        conditionals: false,
+        booleans: false,
+        unused: false,
+        if_return: false,
+        join_vars: false,
+        drop_console: false,
+        drop_debugger: false
       },
-      mangle: {
-        except: ['$super', '$', 'exports', 'require']
-      },
+      mangle: false,
       output: {
         comments: false
       }
     }),
+    function() {
+      this.plugin('watch-run', function(watching, callback) {
+        console.log();
+        console.log('Recompiling assets starting ' + new Date()
+                .timeNow() + "...");
+        callback();
+      })
+    },
     new webpack.DefinePlugin({
       'process.env': {
         'NODE_ENV': JSON.stringify('production'),
