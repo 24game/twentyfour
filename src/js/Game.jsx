64c6f8f1038061f52f2755cb5e1abc3fd2ad19d7
Sebastian Liu <sebastian.liu@gmail.com>
2016-01-10 14:55:41 -0800

    Semi-working, needs simplification for cycleOperator.

--- src/js/Game.jsx
+++ src/js/Game.jsx
@@ -24,8 +24,35 @@ var Game = React.createClass({
   },
 
   // Cycles through the operators in succession.
-  cycleOperator: function() {
-    var currentOperatorIndex = this.getCurrentOperatorIndex(this.state.firstOperatorState);
+  cycleFirstOperator: function(event) {
+    var currentOperatorIndex = this.getCurrentOperatorIndex(event.currentTarget);
+    console.log(event.currentTarget);
+    var nextOperatorIndex = (currentOperatorIndex + 1) % (this.props.possibleOperators.length);
+    this.setState({
+      firstOperatorState: this.props.possibleOperators[nextOperatorIndex]
+    });
+  },
+
+  cycleSecondOperator: function() {
+    var currentOperatorIndex = this.getCurrentOperatorIndex(this.state.secondOperatorState);
+    var nextOperatorIndex = (currentOperatorIndex + 1) % (this.props.possibleOperators.length);
+    this.setState({
+      secondOperatorState: this.props.possibleOperators[nextOperatorIndex]
+    });
+  },
+
+  cycleThirdOperator: function() {
+    var currentOperatorIndex = this.getCurrentOperatorIndex(this.state.thirdOperatorState);
+    var nextOperatorIndex = (currentOperatorIndex + 1) % (this.props.possibleOperators.length);
+    this.setState({
+      thirdOperatorState: this.props.possibleOperators[nextOperatorIndex]
+    });
+  },
+
+  cycleOperator: function(operator) {
+    var currentOperatorIndex = this.getCurrentOperatorIndex(operator);
+    console.log(operator);
+    console.log(currentOperatorIndex);
     var nextOperatorIndex = (currentOperatorIndex + 1) % (this.props.possibleOperators.length);
     console.log(nextOperatorIndex);
     this.setState({
@@ -33,23 +60,45 @@ var Game = React.createClass({
     });
   },
 
+  computeResult: function() {
+
+  },
+
   // Returns the index of the current operator.
   getCurrentOperatorIndex: function(currentOperator) {
     return this.props.possibleOperators.indexOf(currentOperator);
   },
 
+  cleanOperators(stringToClean) {
+    return stringToClean.replace(/×+/g, '*').replace(/÷+/g, '/').replace(/−+/g, '-');
+  },
+
   render: function() {
+    console.log('3' + this.state.firstOperatorState + '3')
+    console.log(eval('3' + this.cleanOperators(this.state.firstOperatorState) + '3'));
     return (
       <section className="flexible rows horizontally-centered vertically-centered game">
         <Tile value={3} />
         <Operator
           operator={this.state.firstOperatorState}
           possibleOperators={this.props.possibleOperators}
-          cycleOperator={this.cycleOperator}
+          cycleOperator={this.cycleFirstOperator}
           />
-        <div>
-          {this.state.firstOperatorState}
-        </div>
+        <Tile value={3} />
+        <Operator
+          operator={this.state.secondOperatorState}
+          possibleOperators={this.props.possibleOperators}
+          cycleOperator={this.cycleSecondOperator}
+          />
+        <Tile value={3} />
+        <Operator
+          operator={this.state.thirdOperatorState}
+          possibleOperators={this.props.possibleOperators}
+          cycleOperator={this.cycleThirdOperator}
+          />
+        <Tile value={3} />
+        <EqualsSign />
+        <Result value={this.state.thirdOperatorState}/>
       </section>
     );
   }
