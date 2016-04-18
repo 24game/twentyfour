b0ad11520ec105b3204e6cc2170389cf561765a8
Sebastian Liu <sebastian.liu@gmail.com>
2016-04-17 18:35:08 -0700

    Adding share buttons.

--- src/js/About.jsx
+++ src/js/About.jsx
@@ -6,9 +6,9 @@ class About extends React.Component {
     return (
       <section className="centered footer-details">
         <p>
-          <a href="https://github.com/24game/twentyfour">TwentyFour</a> was developed by&nbsp;
-          <a href="https://github.com/jasonpang">Jason Pang</a> and&nbsp;
-          <a href="https://github.com/cbass10liu">Sebastian Liu</a>!
+          <a href="https://github.com/24game/twentyfour" target="_blank">TwentyFour</a> was developed by&nbsp;
+          <a href="https://github.com/jasonpang" target="_blank">Jason Pang</a> and&nbsp;
+          <a href="https://github.com/cbass10liu" target="_blank">Sebastian Liu</a>!
         </p>
       </section>
     );
