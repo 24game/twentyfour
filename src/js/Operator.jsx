f773a67a99d5b638394697fe54c924ae83420b76
Jason Pang <jason@onesignal.com>
2016-04-17 17:53:40 -0700

    Fix operator hover effect for mobile and desktop
    On mobile, tapping an operator would position the "invisible" cursor
    over the operator, causing an indefinitely lasting hover effect. The
    solution is to remove application of the hover effect from a standard
    CSS :hover rule to a custom CSS class that can be added and removed via
    JavaScript. Hooks for mouse enter, leave, and move, and hooks for touch
    start, move, and end, are implemented to add and remove this CSS class
    such that the hover class appears to be applied correctly on both mobile
    and desktop devices.

--- src/js/Operator.jsx
+++ src/js/Operator.jsx
@@ -1,17 +1,77 @@
 import React from 'react';
+import Utils from './utils.js';
 
 class Operator extends React.Component {
 
   constructor(props) {
     super(props);
-    // this.cycleOperatorFn = this.cycleOperatorFn.bind(this);
+    this.onTouchStart = this.onTouchStart.bind(this);
+    this.onMouseEnter = this.onMouseEnter.bind(this);
+    this.onMouseLeave = this.onMouseLeave.bind(this);
+    this.onTouchMove = this.onTouchMove.bind(this);
+    this.onMouseDown = this.onMouseDown.bind(this);
+    this.onMouseUp = this.onMouseUp.bind(this);
+    this.onClick = this.onClick.bind(this);
+    this.state = {};
+  }
+
+  onTouchStart(e) {
+    this.beginHoverEffect();
+  }
+
+  onMouseEnter(e) {
+    console.log('Called onMouseEnter.');
+    this.beginHoverEffect();
+  }
+
+  onMouseLeave(e) {
+    this.endHoverEffect();
+  }
+
+  onTouchMove(e) {
+    this.endHoverEffect();
+  }
+
+  onMouseDown(e) {
+    this.beginHoverEffect();
+  }
+
+  onMouseUp(e) {
+    this.endHoverEffect();
+  }
+
+  onClick(e) {
+    this.endHoverEffect();
+    this.performClick();
+  }
+
+  performClick() {
+    console.log(`Called %cperformClick on tile index ${this.props.index} with current operator ${this.props.operator}.`, Utils.getConsoleStyle('code'));
+    this.props.cycleOperatorFn(this.props.index, this.props.operator);
+  }
+
+  beginHoverEffect() {
+    console.log('Called %cbeginHoverEffect.', Utils.getConsoleStyle('code'));
+    this.setState({hovering: true});
+  }
+
+  endHoverEffect() {
+    console.log('Called %cendHoverEffect.', Utils.getConsoleStyle('code'));
+    this.setState({hovering: false});
   }
 
   render() {
+    let isHovering = this.state.hovering;
     return (
       <div
-        className="operator-tile"
-        onClick={this.props.cycleOperatorFn.bind(null, this.props.index, this.props.operator)}
+        className={`operator-tile ${isHovering ? 'operator-hover' : ''}`}
+        onTouchStart={this.onTouchStart}
+        onMouseEnter={this.onMouseEnter}
+        onMouseLeave={this.onMouseLeave}
+        onTouchMove={this.onTouchMove}
+        onMouseDown={this.onMouseDown}
+        onMouseUp={this.onMouseUp}
+        onClick={this.onClick}
         zIndex="1">
         <span className="unselectable operator"> {this.props.operator} </span>
       </div>
