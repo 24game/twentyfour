0b3110f92c8ed586ad91cb4dc722d3ea71b5bd4d
Sebastian Liu <sebastian.liu@gmail.com>
2016-05-05 20:22:38 -0700

    Moving 'default-cursor'  to 'div' level to include the subtitle.

--- src/js/Title.jsx
+++ src/js/Title.jsx
@@ -4,8 +4,8 @@ class Title extends React.Component {
 
   render() {
     return (
-      <div className="unselectable centered title">
-        <h1 className="default-cursor uppercase">Twenty Four</h1>
+      <div className="unselectable default-cursor centered title">
+        <h1 className="uppercase">Twenty Four</h1>
         <h3>use&nbsp;
             <strong className="larger subtitle-operators">
               <code id="plus">+</code>
